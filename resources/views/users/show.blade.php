318e14b58d4b4eb96d85725a6e453e89c8dd0f14
bhaumik-ranpariya <22amtics216@gmail.com>
2026-07-14 18:33:14 +0530

    Fix layout break on user show page caused by extra closing div

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -73,7 +73,6 @@
                             </div>
                         </div>
                     </div>
-                    </div>
                 </div>
 
                 @if(!empty($user->page_permissions))
